130075b8cd5a10ba3dff53fcd8759e7a7b3b14b0
Aaron Michael Kippins <AKippins@hotmail.com>
2021-09-23 10:44:49 -0400

    Updates from Lesson 4

---
+++ QuickDemos/Lesson 4/script.js
@@ -0,0 +1,11 @@
+function alertPeople () {
+    alert("Hello");
+}
+
+function changeBorderColor () {
+    var content = document.getElementById("content");
+    content.style.border = "10px solid blue"
+}
+// while (true) {
+//     alertPeople();
+// }
